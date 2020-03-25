cbc31db0a21b7f74fdb15f8002b721ad554760b6
MTimon <47274597+MTimon@users.noreply.github.com>
2020-03-25 20:34:52 +0100

    Update WSconfig.h
    added OPEN steps in config file

--- WSconfig.h
+++ WSconfig.h
@@ -4,4 +4,7 @@
 #define TOKEN "TOKEN"
 #define WIFI_SSD "WLAN"         //network ssid
 #define WIFI_PASSWD "PASSWORD"     //password
+
+#define OPEN 15000   // set steps to fully open door
+
 #endif
